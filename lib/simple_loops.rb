58cb3c2a13fe4d2cef289bfb4eb6cd7a8cc3cc4c
Andrew Park <acpark123@gmail.com>
2020-06-05 04:45:31 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -2,7 +2,7 @@
 
 def loop_message_five_times(message)
   i = 0
-  while i < 5
+  while i < 5 do
     puts message
     i += 1
   end
@@ -10,7 +10,7 @@ end
 
 def loop_message_n_times(message, integer)
   i = 0
-  while i < integer
+  while i < integer do
     puts message
     i += 1
   end
@@ -18,7 +18,7 @@ end
 
 def output_array(array)
   i = 0
-  while i < array.length
+  while i < array.length do
     puts array[i]
     i += 1
   end
@@ -27,7 +27,7 @@ end
 def return_string_array(array)
   i = 0
   array_string = []
-  while i < array.length
+  while i < array.length do
     array_string.push(array[i].to_s)
     i += 1
   end
